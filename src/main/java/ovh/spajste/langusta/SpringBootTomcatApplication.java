994d14ea4da360039d1d589db0d5526abf4bb9fb
Sebastian Madejski <madejski.s97@gmail.com>
2018-10-09 17:59:18 +0200

    add Tomcat servlet entry point

---
+++ src/main/java/ovh/spajste/langusta/SpringBootTomcatApplication.java
@@ -0,0 +1,8 @@
+package ovh.spajste.langusta;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class SpringBootTomcatApplication extends SpringBootServletInitializer {
+}
