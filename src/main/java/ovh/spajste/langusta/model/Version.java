e12a08f898fc8d717ac5f22aadacec63f9540503
Sebastian Madejski <madejski.s97@gmail.com>
2018-10-09 23:10:40 +0200

    document Version class and purge unused imports

--- src/main/java/ovh/spajste/langusta/model/Version.java
+++ src/main/java/ovh/spajste/langusta/model/Version.java
@@ -1,8 +1,5 @@
 package ovh.spajste.langusta.model;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -11,6 +8,13 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
 
+/**
+ * Version is model, that returns info about last commit on Git.
+ * To be used at developement machine, otherwise it won't produce
+ * any useful information at all.
+ *
+ * @author Sebastian Madejski (mwht)
+ */
 public class Version {
     private String commitHash;
     private Date commitDate;
@@ -33,34 +37,60 @@ public class Version {
         }
     }
 
+    /**
+     * Constructor for Version object. Internal use only, use {@link Version#getInstance()} instead.
+     */
     public Version() {
-        Logger logger = LoggerFactory.getLogger(Version.class);
         commitHash = getGitCommandOutput("git log --format=%H -n 1");
         commitAuthor = getGitCommandOutput("git log --format=%an -n 1");
-        logger.debug("now getting commit author, the almighty " + commitAuthor);
         String rawDate = getGitCommandOutput("git log --format=%ad -n 1");
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat("EEE MMM d HH:mm:ss yyyy Z", Locale.ENGLISH);
         try {
             commitDate = simpleDateFormat.parse(rawDate);
-            logger.debug("kewl, date is " + commitDate.toString());
         } catch (Exception e) {
             commitDate = null;
         }
     }
 
+    /**
+     * Get the instance of {@link Version}. If instance does not exist, create one.
+     *
+     * @return instance of {@link Version}
+     */
     public static Version getInstance() {
         if(instance == null) instance = new Version();
         return instance;
     }
 
+    /**
+     * Get last commit date.
+     *
+     * To be removed on production.
+     *
+     * @return Last commit date
+     */
     public Date getCommitDate() {
         return commitDate;
     }
 
+    /**
+     * Get last commit hash.
+     *
+     * To be removed on production.
+     *
+     * @return Last commit hash
+     */
     public String getCommitHash() {
         return commitHash;
     }
 
+    /**
+     * Get last commit author.
+     *
+     * To be removed on production.
+     *
+     * @return Last commit author
+     */
     public String getCommitAuthor() {
         return commitAuthor;
     }
