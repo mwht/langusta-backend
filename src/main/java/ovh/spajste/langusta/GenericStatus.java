745956f391f5c32c96966ef1211ae46e894205ab
Sebastian Madejski <madejski.s97@gmail.com>
2018-10-10 17:20:27 +0200

    add GenericStatus class and add it to VersionController

---
+++ src/main/java/ovh/spajste/langusta/GenericStatus.java
@@ -0,0 +1,31 @@
+package ovh.spajste.langusta;
+
+public class GenericStatus {
+
+    public enum GenericState {
+        STATUS_SUCCESS,
+        STATUS_ERROR
+    }
+
+    private GenericState state;
+    private String additionalInfo;
+    private Object payload;
+
+    public GenericStatus(GenericState state, String additionalInfo, Object payload) {
+        this.state = state;
+        this.additionalInfo = additionalInfo;
+        this.payload = payload;
+    }
+
+    public String getAdditionalInfo() {
+        return additionalInfo;
+    }
+
+    public GenericState getState() {
+        return state;
+    }
+
+    public Object getPayload() {
+        return payload;
+    }
+}
--- src/main/java/ovh/spajste/langusta/controller/VersionController.java
+++ src/main/java/ovh/spajste/langusta/controller/VersionController.java
@@ -3,11 +3,12 @@ package ovh.spajste.langusta.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ovh.spajste.langusta.model.Version;
+import ovh.spajste.langusta.GenericStatus;
 
 @RestController
 public class VersionController {
     @RequestMapping("/version")
-    public Version getVersion() {
-        return Version.getInstance();
+    public GenericStatus getVersion() {
+        return new GenericStatus(GenericStatus.GenericState.STATUS_SUCCESS, null, Version.getInstance());
     }
 }
