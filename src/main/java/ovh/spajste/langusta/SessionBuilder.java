ac1f45c3de1c9fb50fd2f857062b7575dd959a39
Sebastian Madejski <madejski.s97@gmail.com>
2018-10-26 18:23:04 +0200

    remove unused imports

--- src/main/java/ovh/spajste/langusta/SessionBuilder.java
+++ src/main/java/ovh/spajste/langusta/SessionBuilder.java
@@ -6,8 +6,6 @@ import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.auth0.jwt.exceptions.TokenExpiredException;
 import com.auth0.jwt.interfaces.DecodedJWT;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import ovh.spajste.langusta.entity.Session;
 import ovh.spajste.langusta.entity.User;
 import ovh.spajste.langusta.repository.UserRepository;
