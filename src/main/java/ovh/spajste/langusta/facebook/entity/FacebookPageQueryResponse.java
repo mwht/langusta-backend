d8463befce463aadc31bbb896e4d96de2d849a46
Sebastian Madejski <madejski.s97@gmail.com>
2018-10-28 22:51:31 +0100

    add dropping unknown JSON fields in FacebookPageQueryResponse class

--- src/main/java/ovh/spajste/langusta/facebook/entity/FacebookPageQueryResponse.java
+++ src/main/java/ovh/spajste/langusta/facebook/entity/FacebookPageQueryResponse.java
@@ -1,5 +1,8 @@
 package ovh.spajste.langusta.facebook.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class FacebookPageQueryResponse {
     class FacebookAccounts {
         class FacebookAccountsData {
