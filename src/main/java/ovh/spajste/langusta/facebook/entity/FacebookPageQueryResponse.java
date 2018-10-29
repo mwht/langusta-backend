29ffa2a980bcbb7d96de090328bbf4f38b5bbf98
Sebastian Madejski <madejski.s97@gmail.com>
2018-10-29 09:10:30 +0100

    fix JSON structure in FacebookPageQueryResponse

--- src/main/java/ovh/spajste/langusta/facebook/entity/FacebookPageQueryResponse.java
+++ src/main/java/ovh/spajste/langusta/facebook/entity/FacebookPageQueryResponse.java
@@ -5,6 +5,57 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class FacebookPageQueryResponse {
     class FacebookAccounts {
+        class FacebookPaging {
+            class FacebookCursors {
+                String before;
+                String after;
+
+                public FacebookCursors() {
+                    this(null, null);
+                }
+
+                public FacebookCursors(String before, String after) {
+                    this.before = before;
+                    this.after = after;
+                }
+
+                public String getBefore() {
+                    return before;
+                }
+
+                public String getAfter() {
+                    return after;
+                }
+
+                public void setBefore(String before) {
+                    this.before = before;
+                }
+
+                public void setAfter(String after) {
+                    this.after = after;
+                }
+
+            }
+            private FacebookCursors cursors;
+
+            public FacebookPaging() {
+                this(null);
+            }
+
+            public FacebookPaging(FacebookCursors facebookCursors) {
+                this.cursors = facebookCursors;
+            }
+
+            public FacebookCursors getCursors() {
+                return cursors;
+            }
+
+            public void setCursors(FacebookCursors cursors) {
+                this.cursors = cursors;
+            }
+        }
+        private FacebookPaging paging;
+
         private FacebookBasicPageInfo[] data;
 
         public FacebookAccounts() {
@@ -22,76 +73,30 @@ public class FacebookPageQueryResponse {
         public void setData(FacebookBasicPageInfo[] data) {
             this.data = data;
         }
+
+        public FacebookPaging getPaging() {
+            return paging;
+        }
+
+        public void setPaging(FacebookPaging paging) {
+            this.paging = paging;
+        }
     }
     private FacebookAccounts accounts;
 
-    class FacebookPaging {
-        class FacebookCursors {
-            String before;
-            String after;
-
-            public FacebookCursors() {
-                this(null, null);
-            }
-
-            public FacebookCursors(String before, String after) {
-                this.before = before;
-                this.after = after;
-            }
-
-            public String getBefore() {
-                return before;
-            }
-
-            public String getAfter() {
-                return after;
-            }
-
-            public void setBefore(String before) {
-                this.before = before;
-            }
-
-            public void setAfter(String after) {
-                this.after = after;
-            }
-        }
-        private FacebookCursors cursors;
-
-        public FacebookPaging() {
-            this(null);
-        }
-
-        public FacebookPaging(FacebookCursors facebookCursors) {
-            this.cursors = facebookCursors;
-        }
-
-        public FacebookCursors getCursors() {
-            return cursors;
-        }
-
-        public void setCursors(FacebookCursors cursors) {
-            this.cursors = cursors;
-        }
-    }
-    private FacebookPaging paging;
     private String id;
 
     public FacebookPageQueryResponse() {
 
     }
 
-    public FacebookPageQueryResponse(FacebookAccounts facebookAccounts, FacebookPaging facebookPaging, String id) {
+    public FacebookPageQueryResponse(FacebookAccounts facebookAccounts, String id) {
         this.accounts = facebookAccounts;
-        this.paging = facebookPaging;
         this.id = id;
     }
 
     public FacebookAccounts getAccounts() {
         return accounts;
-    }
-
-    public FacebookPaging getPaging() {
-        return paging;
     }
 
     public String getId() {
@@ -100,10 +105,6 @@ public class FacebookPageQueryResponse {
 
     public void setAccounts(FacebookAccounts accounts) {
         this.accounts = accounts;
-    }
-
-    public void setPaging(FacebookPaging paging) {
-        this.paging = paging;
     }
 
     public void setId(String id) {
