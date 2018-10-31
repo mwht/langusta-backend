8908950742f4d2f3cf027cd00631cf81a7ad7e43
Sebastian Madejski <madejski.s97@gmail.com>
2018-10-31 14:45:52 +0100

    add debug log to /facebook/webhook

--- src/main/java/ovh/spajste/langusta/facebook/controller/FacebookPageWebhookController.java
+++ src/main/java/ovh/spajste/langusta/facebook/controller/FacebookPageWebhookController.java
@@ -1,16 +1,24 @@
 package ovh.spajste.langusta.facebook.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.logging.Logger;
 
 @RestController
 public class FacebookPageWebhookController {
     @GetMapping("/facebook/webhook")
     public String facebookHandshakeWebhookHandler(@RequestParam("hub.mode") String hubMode, @RequestParam("hub.challenge") String hubChallenge, @RequestParam("hub.verify_token") String hubVerifyToken, HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) {
         return hubChallenge;
+    }
+
+    @GetMapping("/facebook/webhook")
+    public String  facebookHandshakeWebhookHandler(@RequestBody String postData, HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) {
+        Logger.getAnonymousLogger().info(postData);
+        return "OK";
     }
 }
