b0ef33aac3ac93abfde6a32f9104367d3eae4913
Sebastian Madejski <madejski.s97@gmail.com>
2018-11-09 14:00:33 +0100

    add POST /contest endpoint

--- src/main/java/ovh/spajste/langusta/controller/ContestController.java
+++ src/main/java/ovh/spajste/langusta/controller/ContestController.java
@@ -1,16 +1,57 @@
 package ovh.spajste.langusta.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import ovh.spajste.langusta.ContestParameters;
 import ovh.spajste.langusta.GenericStatus;
+import ovh.spajste.langusta.SessionBuilder;
+import ovh.spajste.langusta.entity.Contest;
+import ovh.spajste.langusta.entity.Session;
 import ovh.spajste.langusta.repository.ContestRepository;
+import ovh.spajste.langusta.repository.PlatformRepository;
+import ovh.spajste.langusta.repository.SessionRepository;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @RestController
 public class ContestController {
 
     @Autowired
     private ContestRepository contestRepository;
+
+    @Autowired
+    private PlatformRepository platformRepository;
+
+    @Autowired
+    private SessionRepository sessionRepository;
+
+    @Value("${langusta.hmac-secret}")
+    private String langustaHmacSecret;
+
+    @PostMapping(path = "/contest", consumes = {MediaType.APPLICATION_JSON_VALUE})
+    public GenericStatus addNewContest(@RequestBody ContestParameters contestParameters, HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) {
+        Session session = SessionBuilder.getCurrentSession(langustaHmacSecret, sessionRepository, httpServletRequest);
+        Contest contest = new Contest(
+                null,
+                session.getUser(),
+                contestParameters.getTitle(),
+                platformRepository.findPlatformByCanonicalName(contestParameters.getPlatform()).get(),
+                contestParameters.getPostLink(),
+                null,
+                null,
+                0,
+                null
+        );
+        contestRepository.save(contest);
+        httpServletResponse.setStatus(201);
+        return GenericStatus.createSuccessfulStatus(null);
+    }
 
     @GetMapping("/contest/all")
     public GenericStatus getAllContests() {
