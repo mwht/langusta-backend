81ee3d0af3ed35a646574d6ed30bb504f9b4345b
Sebastian Madejski <madejski.s97@gmail.com>
2018-11-09 13:42:53 +0100

    add activationCode as ignored field

--- src/main/java/ovh/spajste/langusta/entity/User.java
+++ src/main/java/ovh/spajste/langusta/entity/User.java
@@ -5,7 +5,7 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import javax.persistence.*;
 
 @Entity
-@JsonIgnoreProperties({"pass"})
+@JsonIgnoreProperties({"pass","activationCode"})
 public class User {
 
     @Id
