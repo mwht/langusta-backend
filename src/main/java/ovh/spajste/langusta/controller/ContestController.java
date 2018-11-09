299c26bdfec0336d2bc48cbdccaaab3ae5f2101b
Sebastian Madejski <madejski.s97@gmail.com>
2018-11-09 13:36:54 +0100

    add /contest/all endpoint

--- src/main/java/ovh/spajste/langusta/controller/ContestController.java
+++ src/main/java/ovh/spajste/langusta/controller/ContestController.java
@@ -1,8 +1,22 @@
 package ovh.spajste.langusta.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import ovh.spajste.langusta.GenericStatus;
+import ovh.spajste.langusta.repository.ContestRepository;
 
 @RestController
 public class ContestController {
+
+    @Autowired
+    private ContestRepository contestRepository;
+
+    @GetMapping("/contest/all")
+    public GenericStatus getAllContests() {
+        /* TODO: probably security! */
+        return GenericStatus.createSuccessfulStatus(contestRepository.findAll());
+    }
+
 
 }
