c6a27ae27fa243e4ce5fce245d6c14d1bf1e0804
Sebastian Madejski <madejski.s97@gmail.com>
2018-11-09 16:21:12 +0100

    enable scheduling to make system cron-independent

--- src/main/java/ovh/spajste/langusta/Bootstrap.java
+++ src/main/java/ovh/spajste/langusta/Bootstrap.java
@@ -2,8 +2,10 @@ package ovh.spajste.langusta;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class Bootstrap {
     public static void main(String[] args) {
         SpringApplication.run(Bootstrap.class, args);
