127b4735c295211249ecc901cc98cf014f263534
Sebastian Madejski <madejski.s97@gmail.com>
2018-11-23 23:26:28 +0100

    add post ID extraction from posted content URL

--- src/main/java/ovh/spajste/langusta/facebook/FacebookContestHandler.java
+++ src/main/java/ovh/spajste/langusta/facebook/FacebookContestHandler.java
@@ -6,10 +6,24 @@ import ovh.spajste.langusta.entity.Contest;
 import ovh.spajste.langusta.facebook.service.FacebookService;
 
 import java.util.logging.Logger;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class FacebookContestHandler implements ContestHandler {
 
     public void doContest(Contest contest) {
-        Logger.getAnonymousLogger().info("FB contest handler - ! TODO !");
+
+        // https://www.facebook.com/[dont care]/posts/[post id]
+        Pattern linkRegex = Pattern.compile("^https://www.facebook.com/(.*)/posts/(.*)");
+        String postId = null;
+
+        Matcher regexMatcher = linkRegex.matcher(contest.getPostLink());
+        try {
+            while (regexMatcher.find())
+                postId = regexMatcher.group(2);
+        } catch (NullPointerException npe) {
+            throw new SecurityException("Unparsable Facebook object supplied!");
+        }
+        Logger.getAnonymousLogger().info("FB contest handler - ! TODO ! (contest link "+postId+")");
     }
 }
