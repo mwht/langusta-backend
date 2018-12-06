e5da56e0607b9a7935e1548fcb44081be1c1d1b3
Sebastian Madejski <madejski.s97@gmail.com>
2018-12-06 20:33:39 +0100

    add saving YouTube access token in case of successful authorization

--- src/main/java/ovh/spajste/langusta/youtube/YoutubeAuthCallbackServlet.java
+++ src/main/java/ovh/spajste/langusta/youtube/YoutubeAuthCallbackServlet.java
@@ -8,7 +8,12 @@ import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
 import com.google.api.client.http.GenericUrl;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.jackson2.JacksonFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import ovh.spajste.langusta.SessionBuilder;
+import ovh.spajste.langusta.entity.Session;
+import ovh.spajste.langusta.youtube.entity.YoutubeAccessToken;
+import ovh.spajste.langusta.youtube.repository.YoutubeAccessTokenRepository;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -16,6 +21,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.Collections;
+import java.util.Date;
 
 @WebServlet("/youtube/authCallback")
 public class YoutubeAuthCallbackServlet extends AbstractAuthorizationCodeCallbackServlet {
@@ -26,10 +32,23 @@ public class YoutubeAuthCallbackServlet extends AbstractAuthorizationCodeCallbac
     @Value("${langusta.google.oauth.clientSecret}")
     private String langustaClientSecret;
 
+    @Autowired
+    private YoutubeAccessTokenRepository youtubeAccessTokenRepository;
+
+    @Autowired
+    private SessionBuilder sessionBuilder;
+
     @Override
     protected void onSuccess(HttpServletRequest req, HttpServletResponse resp, Credential credential)
             throws ServletException, IOException {
-        resp.sendRedirect("/home");
+        try {
+            Session session = sessionBuilder.getCurrentSession(req);
+            YoutubeAccessToken youtubeAccessToken = new YoutubeAccessToken(null, session.getUser().getId(), credential.getAccessToken(), new Date(((long)credential.getExpirationTimeMilliseconds()) * 1000L));
+            youtubeAccessTokenRepository.save(youtubeAccessToken);
+            resp.sendRedirect("/home");
+        } catch (Exception e) {
+            resp.sendRedirect("/error");
+        }
     }
 
     @Override
