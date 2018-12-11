c12a1cd1fa76ec3d5772ccaaed3ee1c5923fbf66
Sebastian Madejski <madejski.s97@gmail.com>
2018-12-11 12:05:51 +0100

    add DashboardController

--- src/main/java/ovh/spajste/langusta/repository/ContestRepository.java
+++ src/main/java/ovh/spajste/langusta/repository/ContestRepository.java
@@ -2,6 +2,10 @@ package ovh.spajste.langusta.repository;
 
 import org.springframework.data.repository.CrudRepository;
 import ovh.spajste.langusta.entity.Contest;
+import ovh.spajste.langusta.entity.User;
+
+import java.util.List;
 
 public interface ContestRepository extends CrudRepository<Contest, Integer> {
+    List<Contest> findContestByUser(User user);
 }
